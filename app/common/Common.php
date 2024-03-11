415fb1cc0b4d20f59c0e61f5508fa82eed9edda0
joeycyy <1029122023@qq.com>
2024-03-11 14:31:09 +0800

    增加注释

--- app/common/Common.php
+++ app/common/Common.php
@@ -1,6 +1,8 @@
 <?php
 /*
  * Common公共特征
+ * 5.40新增
+ * 为php单继承语言而准备的一种代码复用机制
  * */
 namespace app\common;
 
@@ -8,7 +10,7 @@ use think\Response;
 
 trait Common
 {
-    public function ReturnApi($data = [], $msg = '数据为空！', $code = 201, $type = 'json')
+    public function returnApi($data = [], $msg = '数据为空！', $code = 201, $type = 'json'): Response
     {
         $result = [
             'code' => $code,
