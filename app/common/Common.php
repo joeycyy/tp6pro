3cc2ca9f4430b5108e7f6507602739f54bc63a6a
joeycyy <1029122023@qq.com>
2024-03-14 13:38:01 +0800

    增加日志，增加status状态码

--- app/common/Common.php
+++ app/common/Common.php
@@ -4,41 +4,46 @@
  * 5.40新增
  * 为php单继承语言而准备的一种代码复用机制
  * */
+
 namespace app\common;
 
+use think\facade\Log;
 use think\Response;
 
-trait Common
-{
+trait Common {
     /*
      * 用于和前端交互，返回失败的Response数据
      * */
-    public function success($msg, $data = [], $code = 1, $type = 'json'): Response
-    {
+    public function success($msg, $data = [], $code = 1, $type = 'json'): Response {
         $result = [
             'code' => $code,
             'msg' => $msg,
-            'data' => $data
+            'data' => $data,
+            'status' => 'SUCCESS'
         ];
+        Log::debug($msg, $data);
         return Response::create($result, $type);
     }
+
     /*
      * 用于和前端交互，返回失败的Response数据
      * */
-    public function error($msg, $data = [], $code = 0, $type = 'json'): Response
-    {
+    public function error($msg, $data = [], $code = 0, $type = 'json'): Response {
         $result = [
             'code' => $code,
             'msg' => $msg,
-            'data' => $data
+            'data' => $data,
+            'status' => 'FAIL'
         ];
+        Log::debug($msg, $data);
         return Response::create($result, $type);
     }
+
     /*
      * 后端自己的交互，返回一个数组
      * */
-    public function returnApi($status, $msg = '', $data=[]):array
-    {
+    public function returnApi($status, $msg = '', $data = []): array {
+        Log::debug($msg, $data);
         return array(
             'status' => $status,
             'data' => $data,
