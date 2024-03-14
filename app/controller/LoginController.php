637ef6759cc4d1925ce70715b98a699387af268c
joeycyy <1029122023@qq.com>
2024-03-14 13:42:51 +0800

    登录首页api提交

--- app/controller/LoginController.php
+++ app/controller/LoginController.php
@@ -3,14 +3,78 @@
 namespace app\controller;
 
 use app\BaseController;
+use app\model\admin\Admin;
+use think\captcha\facade\Captcha;
+use think\facade\Log;
+use think\Response;
+use think\Response\View;
 
 class LoginController extends BaseController {
-    public function index() {
+    // 显示登录模板
+    public function index(): View {
+        Log::debug('登录Index');
         $view_data['admin_title'] = 'php管理后台';
         return view('login', $view_data);
     }
 
-    public function loginAct() {
+    // 登录操作
+    public function loginAct(): Response {
+        Log::debug('登录操作');
+        // 获取前端传入参数
+            $data = request()->param();
+        // 校验输入的验证码是否正确
+        if (!captcha_check($data['vercode'])) {
+//            return $this->error('验证码输入错误');
+        }
 
+        $username = $data['username'];
+        if (empty($username)) {
+            return $this->error('管理员账号为空');
+        }
+
+        // 登录验证
+        $admin = new Admin();
+        $admin_info = $admin->getAdminInfoByAdminName($username);
+
+        // 如果找不到对应的管理员账号信息
+        if (empty($admin_info)) {
+            return $this->error('登录失败,账号或密码错误！');
+        }
+        // 判断密码是否正确
+        if (!password_verify($data['password'], $admin_info['admin_pwd'])) {
+            // TODO 增加日记
+            return $this->error('登录失败,账号或密码错误！');
+        }
+
+        // 判断账号状态
+        if ($admin_info['admin_status'] == 2) {
+            return $this->error('登录失败,账号已被限制登录！');
+        } elseif ($admin_info['admin_status'] == 3) {
+            return $this->error('登录失败,账号已被冻结！');
+        }
+
+        // 判断当天是否登录错误超过10次 TODO
+
+        // 记录登录日志 TODO
+
+        // 记录session和cookie TODO
+        session('admin_id', $admin_info['admin_id']);
+        session('admin_name', $admin_info['admin_name']);
+        session('admin_shell', md5("LJAF&AFA" . $admin_info['admin_id'] . $admin_info['admin_pwd']));
+
+        // 保存密码，则保存cookie15天
+        if ($admin_info['remember'] == 1) {
+            cookie('admin_id', $admin_info['admin_id'], 1296000);
+            cookie('admin_name', $admin_info['admin_name'], 1296000);
+            cookie('admin_shell', md5("LJAF&AFA" . $admin_info['admin_id'] . $admin_info['admin_pwd']), 1296000);
+        }
+
+        return $this->success('登录成功！');
+    }
+
+    // 登录验证码图片生成
+    public function loginYzm(): Response {
+        Log::debug('验证码生成');
+        return Captcha::create();
     }
 }
--- app/model/admin/Admin.php
+++ app/model/admin/Admin.php
@@ -4,8 +4,7 @@ namespace app\model\admin;
 
 use think\Model;
 
-class Admin extends Model
-{
+class Admin extends Model {
     protected $pk = 'admin_id';
 
     // 设置字段信息
@@ -22,14 +21,12 @@ class Admin extends Model
     ];
 
     // 模型初始化
-    protected static function init()
-    {
+    protected static function init() {
 
     }
 
     // 插入数据-新增一个管理员
-    public function addItem($data)
-    {
+    public function addItem($data) {
         // 不需要在model层进行数据校验，一般在前端和controller进行拦截，默认model层都是合法的数据
         $data['admin_pwd'] = password_hash($data['pass'], PASSWORD_DEFAULT);
         $data['add_datetime'] = date("Y-m-d H:i:s");
@@ -46,8 +43,7 @@ class Admin extends Model
     }
 
     // 获取角色权限列表
-    public function selectAllRole()
-    {
+    public function selectAllRole() {
         $roles = $this->select();
         echo $this->getLastSql();
         return $roles;
@@ -56,8 +52,17 @@ class Admin extends Model
     /*
      * 根据admin_id查找管理员信息
      * */
-    public function getAdminInfo($admin_id)
-    {
+    public function getAdminInfo($admin_id) {
         return $this->where('admin_id', $admin_id)->findOrEmpty();
+    }
+
+    /*
+     * 根据admin_name查找管理员信息
+     * */
+    public function getAdminInfoByAdminName($admin_name) {
+        return $this->where('admin_name', $admin_name)->findOrEmpty();
+    }
+    public function checkLogin() {
+
     }
 }
