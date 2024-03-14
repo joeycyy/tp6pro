0186619d8a4eed4b011ea9349ec10e513428030d
joeycyy <1029122023@qq.com>
2024-03-14 13:40:32 +0800

    增加日记config

--- config/log.php
+++ config/log.php
@@ -8,11 +8,11 @@ return [
     // 默认日志记录通道
     'default'      => Env::get('log.channel', 'file'),
     // 日志记录级别
-    'level'        => [],
+    'level'        => ['error', 'debug', 'critical', 'alert', 'emergency'],
     // 日志类型记录的通道 ['error'=>'email',...]
     'type_channel' => [],
     // 关闭全局日志写入
-    'close'        => false,
+    'close'        => true,
     // 全局日志处理 支持闭包
     'processor'    => null,
 
